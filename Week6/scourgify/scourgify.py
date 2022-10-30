3f5c79a8dbc3d2363b012f203ff6d14ad21b92ef
Eric Loc <eloc1385@gmail.com>
2022-10-30 09:30:58 -0700

    completed scourgify problem set

--- Week6/scourgify/scourgify.py
+++ Week6/scourgify/scourgify.py
@@ -13,10 +13,16 @@ elif len(sys.argv) >= 3:
     elif extensionCheck == "csv" and not path.exists(sys.argv[1]):
         sys.exit(f"Could not read {sys.argv[1]}")
     else:
+        output = []
         with open(sys.argv[1], "r") as file:
             reader = csv.DictReader(file)
             for row in reader:
                 splitName = row["name"].split(",")
-                print(splitName[1])
-            
+                output.append({'first': splitName[1].lstrip(), 'last': splitName[0], 'house':row["house"]})
+        
+        with open(sys.argv[2], "w") as file:
+            writer = csv.DictWriter(file, fieldnames=['first', 'last', 'house'])
+            writer.writerow({"first": "first", "last": "last", "house": "house"})
+            for row in output:
+                writer.writerow({"first": row["first"], "last": row["last"], "house": row["house"]})
             
